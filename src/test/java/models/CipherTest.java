688f288a848f10ec3a5fb15ff2b64e57ebd4d7f3
Gladys-nimo <wkanyora10@gmail.com>
2020-11-23 09:39:05 +0300

    added some files and tests

--- src/test/java/models/CipherTest.java
+++ src/test/java/models/CipherTest.java
@@ -19,6 +19,21 @@ public class CipherTest  {
         Cipher testCipher = new Cipher("a",2);
         assertEquals(2,testCipher.getKey());
     }
+    @Test
+    public void newCipher_replaceLetter_b(){
+        Cipher testCipher = new Cipher("a",2);
+        assertEquals("b",testCipher.isEncrypted());
+    }
+    @Test
+    public void newCipher_replaceLetterByUsingAKey_c(){
+        Cipher testCipher = new Cipher("a",2);
+        assertEquals("c",testCipher.isActuallyEncrypted("a",2));
+    }
+    @Test
+    public void newCipher_replaceMultipleLettersUsingKeys_cde(){
+        Cipher testCipher = new Cipher("cde",2);
+        assertEquals("abc",testCipher.toDecrypt("cde",2));
+    }
 
 
 }
